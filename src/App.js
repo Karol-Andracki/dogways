be6b78d60011adf45281b32825cd3e8fae800328
Karol Andracki <andracki@gmail.com>
2019-12-30 23:54:51 +0100

    home images and html structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home'
 
 function App() {
   return (
---
+++ src/components/Home.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import styles from '../styles'
+
+export default function Home() {
+    return (
+        <div>
+            <div className={styles.img1}>
+                <div className={styles.title}>
+                    <span className={styles.spanText}>
+                        Welcome to DogsWays
+            </span>
+                </div>
+            </div>
+            <section className={styles.section}>
+                <h2>section one</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores saepe provident numquam labore maxime voluptates facilis placeat non necessitatibus, eius maiores nisi? In recusandae illo debitis placeat laudantium maiores nobis, laborum rem, ea dicta blanditiis vero facilis sint incidunt earum cupiditate? Consequatur nemo neque hic dignissimos, quos tempore nisi atque!
+        </p>
+            </section>
+            <div className={styles.img2}>
+                <div className={styles.title}>
+                    <span className={styles.spanText}>
+                        Manja Ganja 2
+            </span>
+                </div>
+            </div>
+            <section className={styles.section}>
+                <h2>section two</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores saepe provident numquam labore maxime voluptates facilis placeat non necessitatibus, eius maiores nisi? In recusandae illo debitis placeat laudantium maiores nobis, laborum rem, ea dicta blanditiis vero facilis sint incidunt earum cupiditate? Consequatur nemo neque hic dignissimos, quos tempore nisi atque!
+        </p>
+            </section>
+            <div className={styles.img3}>
+                <div className={styles.title}>
+                    <span className={styles.spanText}>
+                        Manja Ganja 3
+            </span>
+                </div>
+            </div>
+            <section className={styles.section}>
+                <h2>section three</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores saepe provident numquam labore maxime voluptates facilis placeat non necessitatibus, eius maiores nisi? In recusandae illo debitis placeat laudantium maiores nobis, laborum rem, ea dicta blanditiis vero facilis sint incidunt earum cupiditate? Consequatur nemo neque hic dignissimos, quos tempore nisi atque!
+        </p>
+            </section>
+            <div className={styles.img4}>
+                <div className={styles.title}>
+                    <span className={styles.spanText}>
+                        Manja Ganja 4
+            </span>
+                </div>
+            </div>
+        </div>
+    )
+}
