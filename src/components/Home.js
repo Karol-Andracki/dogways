1d1821f1175be803540fe94ad782408d11993161
Karol Andracki <andracki@gmail.com>
2019-12-31 12:25:50 +0100

    logo size adjustements

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,20 +7,22 @@ export default function Home() {
             <div className={styles.img1}>
                 <div className={styles.title}>
                     <span className={styles.spanText}>
-                        Welcome to DogsWays
+                        Welcome to DogsWays <br/>
+                        
+                    
             </span>
                 </div>
             </div>
             <section className={styles.section}>
-                <h2>section one</h2>
+                <h2>Our mission</h2>
                 <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores saepe provident numquam labore maxime voluptates facilis placeat non necessitatibus, eius maiores nisi? In recusandae illo debitis placeat laudantium maiores nobis, laborum rem, ea dicta blanditiis vero facilis sint incidunt earum cupiditate? Consequatur nemo neque hic dignissimos, quos tempore nisi atque!
+                At the DogsWays we place the right relationship between the owner(s) and the dog(s) central. We don’t use any intimidation or dominance-based methods. We passionately believe that the dog’s welfare benefits from increased education and aims to provide this by offering scientifically up-to-date information.
         </p>
             </section>
             <div className={styles.img2}>
                 <div className={styles.title}>
                     <span className={styles.spanText}>
-                        Manja Ganja 2
+                        Our services
             </span>
                 </div>
             </div>
@@ -33,7 +35,7 @@ export default function Home() {
             <div className={styles.img3}>
                 <div className={styles.title}>
                     <span className={styles.spanText}>
-                        Manja Ganja 3
+                        Shop
             </span>
                 </div>
             </div>
@@ -46,10 +48,16 @@ export default function Home() {
             <div className={styles.img4}>
                 <div className={styles.title}>
                     <span className={styles.spanText}>
-                        Manja Ganja 4
+                        Blog
             </span>
                 </div>
             </div>
+            <section className={styles.section}>
+                <h2>section one</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores saepe provident numquam labore maxime voluptates facilis placeat non necessitatibus, eius maiores nisi? In recusandae illo debitis placeat laudantium maiores nobis, laborum rem, ea dicta blanditiis vero facilis sint incidunt earum cupiditate? Consequatur nemo neque hic dignissimos, quos tempore nisi atque!
+        </p>
+            </section>
         </>
     )
 }
